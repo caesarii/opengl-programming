def86b40f99c9bd15802030dd11e7cc14d30dd2f
caesarii <iwangqinghe@gmail.com>
2020-07-27 19:34:20 +0800

    顶点数组, 补充demo

--- 3. 顶点数组/glut/main.c
+++ 3. 顶点数组/glut/main.c
@@ -12,13 +12,94 @@ void
 init(void) {
     glClearColor(0, 0, 0, 0);
     glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
 }
 
 
 void
 drawArrayElement () {
+    
+    GLfloat vertices[] = {
+        0.0, 0.0, 0.0,
+        0.0, 500.0, 0.0,
+        500.0, 500.0, 0.0,
+        500.0, 0.0, 0.0,
+    };
+    
+    // 需要显示指定
+    glShadeModel(GL_SMOOTH);
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+    
+    glVertexPointer(3, GL_FLOAT, 3*sizeof(GLfloat), &vertices[0]);
+    
+   
+    glBegin(GL_QUADS);
+    glArrayElement(0);
+    glArrayElement(1);
+    glArrayElement(2);
+    glArrayElement(3);
+    glEnd();
+    
+}
+
+void
+drawArrayElementBatch () {
+    
+    GLfloat vertices[] = {
+        0.0, 0.0, 0.0,
+        0.0, 500.0, 0.0,
+        500.0, 500.0, 0.0,
+        500.0, 0.0, 0.0,
+    };
+    
+    // 需要显示指定
+    glShadeModel(GL_SMOOTH);
+
+    glEnableClientState(GL_VERTEX_ARRAY);
+    
+    glVertexPointer(3, GL_FLOAT, 3*sizeof(GLfloat), &vertices[0]);
+    
+    GLubyte indices[] = {0, 1, 2, 3};
+    glDrawElements(GL_QUADS, 4, GL_UNSIGNED_BYTE, indices);
+}
+
+void
+drawColorArrayElement () {
+    
+    GLfloat vertices[] = {
+        0.0, 0.0, 0.0,
+        0.0, 500.0, 0.0,
+        500.0, 500.0, 0.0,
+        500.0, 0.0, 0.0,
+    };
+    
+    GLfloat colors[] = {
+        1.0, 0.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0,
+        1.0, 1.0, 1.0,
+    };
+    
+    // 需要显示指定
+    glShadeModel(GL_SMOOTH);
+        
+    // 激活
+    glEnableClientState(GL_COLOR_ARRAY);
+    glEnableClientState(GL_VERTEX_ARRAY);
+    
+    // 指定数组数据
+    glVertexPointer(3, GL_FLOAT, 3*sizeof(GLfloat), &vertices[0]);
+    glColorPointer(3, GL_FLOAT, 3*sizeof(GLfloat), &colors[0]);
+    
+
+    // 索引数组列表, glArrayElement 一次使用一个元素, glDrawElements 根据索引数组使用多个元素
+    // 与上面多次调用 glArrayElement 是等价的
+    GLubyte indices[] = {0, 1, 2, 3};
+    glDrawElements(GL_QUADS, 4, GL_UNSIGNED_BYTE, indices);
+}
+
+void
+drawInterwineArrayElement () {
     // interwined 数组
     GLfloat vertices[] = {
         1.0, 0.0, 0.0, 0.0, 0.0, 0.0,
@@ -38,16 +119,7 @@ drawArrayElement () {
     glColorPointer(3, GL_FLOAT, 6*sizeof(GLfloat), &vertices[0]);
     glVertexPointer(3, GL_FLOAT, 6*sizeof(GLfloat), &vertices[3]);
     
-    // 索引数组元素
-    // glBegin(GL_QUADS);
-    // glArrayElement(0);
-    // glArrayElement(1);
-    // glArrayElement(2);
-    // glArrayElement(3);
-    // glEnd();
-    
-    // 索引数组列表, glArrayElement 一次使用一个元素, glDrawElements 根据索引数组使用多个元素
-    // 与上面多次调用 glArrayElement 是等价的
+   
     GLubyte indices[] = {0, 1, 2, 3};
     glDrawElements(GL_QUADS, 4, GL_UNSIGNED_BYTE, indices);
 }
@@ -56,7 +128,13 @@ void
 render(void) {
     glClear(GL_COLOR_BUFFER_BIT);
     
-    drawArrayElement();
+    // drawArrayElement();
+    
+    // drawArrayElementBatch();
+    
+    // drawColorArrayElement();
+    
+    drawInterwineArrayElement();
     
     glFlush();
 }
